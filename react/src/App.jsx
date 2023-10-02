c40a3bcc870d4c85a20151ce0558084e4478e4e3
Durjay Ghosh <42237029+durjaygp@users.noreply.github.com>
2023-10-02 11:53:25 +0600

    project started

---
+++ react/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import './index.css'
+
+function App() {
+
+  return (
+      <div>
+        <p className="text-3xl font-bold underline bg-purple-500">Durjay Ghosh</p>
+      </div>
+  )
+}
+
+export default App
